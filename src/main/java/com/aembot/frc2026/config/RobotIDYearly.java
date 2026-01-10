9069b46e51e5d1e12124fd574d85619d42e21687
FlameFish <mccoa499@gmail.com>
2026-01-09 21:19:31 -0800

    spotless

--- src/main/java/com/aembot/frc2026/config/RobotIDYearly.java
+++ src/main/java/com/aembot/frc2026/config/RobotIDYearly.java
@@ -1,44 +1,45 @@
 package com.aembot.frc2026.config;
 
+import com.aembot.lib.config.RobotID;
 import java.util.Map;
 
-import com.aembot.lib.config.RobotID;
-
 public enum RobotIDYearly implements RobotID {
-    PRODUCTION("Production Bot"),
-    ;
+  PRODUCTION("Production Bot"),
+  ;
 
-    private final String name;
-    private String macAddress = null;
-    private RobotIDYearly(String name){
-        this.name = name;
-    }
+  private final String name;
+  private String macAddress = null;
 
-    @Override
-    public RobotID withMACAddress(String mac) {
-        this.macAddress = mac;
-        return this;
-    }
+  private RobotIDYearly(String name) {
+    this.name = name;
+  }
 
-    @Override
-    public String getName() {
-        return name;
-    }
+  @Override
+  public RobotID withMACAddress(String mac) {
+    this.macAddress = mac;
+    return this;
+  }
 
-    @Override
-    public String getMACAddress() {
-        return macAddress;
-    }
+  @Override
+  public String getName() {
+    return name;
+  }
 
-    @Override
-    public Map<String, RobotID> getMACToRobot() {
-        return Map.of(
-            "blah:blah:blah:blah:blah:blah", RobotIDYearly.PRODUCTION // TODO When we have bot: get actual MAC adress
+  @Override
+  public String getMACAddress() {
+    return macAddress;
+  }
+
+  @Override
+  public Map<String, RobotID> getMACToRobot() {
+    return Map.of(
+        "blah:blah:blah:blah:blah:blah",
+        RobotIDYearly.PRODUCTION // TODO When we have bot: get actual MAC adress
         );
-    }
+  }
 
-    @Override
-    public RobotID getDefaultRobot() {
-        return PRODUCTION;
-    }
+  @Override
+  public RobotID getDefaultRobot() {
+    return PRODUCTION;
+  }
 }
