fd4b8b560f2811a2910f47911a3d14e4187237fa
FlameFish <mccoa499@gmail.com>
2026-01-16 16:35:15 -0800

    spotless

--- src/main/java/com/aembot/lib/config/subsystems/drive/simulation/DrivetrainSimConfiguration.java
+++ src/main/java/com/aembot/lib/config/subsystems/drive/simulation/DrivetrainSimConfiguration.java
@@ -3,40 +3,41 @@ package com.aembot.lib.config.subsystems.drive.simulation;
 import com.aembot.lib.config.robot.PhysicalConfiguration;
 
 public class DrivetrainSimConfiguration {
-    /** Name of the config  */
-    public String kConfigurationName;
+  /** Name of the config */
+  public String kConfigurationName;
 
-    /** Rate in seconds that the sim will loop */
-    public final double simLoopPeriodS; 
+  /** Rate in seconds that the sim will loop */
+  public final double simLoopPeriodS;
 
-    public PhysicalConfiguration physicalConfiguration;
+  public PhysicalConfiguration physicalConfiguration;
 
-    /** Number of drive motors per swerve modules */
-    public int driveMotorsPerModule;
-    /** Number of steer motors per swerve modules */
-    public int steerMotorsPerModule;
+  /** Number of drive motors per swerve modules */
+  public int driveMotorsPerModule;
 
-    public DrivetrainSimConfiguration(double loopPeriod){
-        this.simLoopPeriodS = loopPeriod;
-    }
+  /** Number of steer motors per swerve modules */
+  public int steerMotorsPerModule;
 
-    public DrivetrainSimConfiguration withPhysicalConfiguration(PhysicalConfiguration config){
-        this.physicalConfiguration = config;
-        return this;
-    }
-    
-    public DrivetrainSimConfiguration withDriveMotorsPerModule(int driveMotorCount) {
-        this.driveMotorsPerModule = driveMotorCount;
-        return this;
-    }
-    
-    public DrivetrainSimConfiguration withSteerMotorsPerModule(int steerMotorCount) {
-        this.steerMotorsPerModule = steerMotorCount;
-        return this;
-    }
+  public DrivetrainSimConfiguration(double loopPeriod) {
+    this.simLoopPeriodS = loopPeriod;
+  }
 
-    public DrivetrainSimConfiguration withName(String name) {
-        this.kConfigurationName = name;
-        return this;
-    }
+  public DrivetrainSimConfiguration withPhysicalConfiguration(PhysicalConfiguration config) {
+    this.physicalConfiguration = config;
+    return this;
+  }
+
+  public DrivetrainSimConfiguration withDriveMotorsPerModule(int driveMotorCount) {
+    this.driveMotorsPerModule = driveMotorCount;
+    return this;
+  }
+
+  public DrivetrainSimConfiguration withSteerMotorsPerModule(int steerMotorCount) {
+    this.steerMotorsPerModule = steerMotorCount;
+    return this;
+  }
+
+  public DrivetrainSimConfiguration withName(String name) {
+    this.kConfigurationName = name;
+    return this;
+  }
 }
