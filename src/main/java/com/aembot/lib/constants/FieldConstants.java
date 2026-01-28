96d3156c7b25cc0baae38cdcf70fb3d11f1cd734
FlameFish <mccoa499@gmail.com>
2026-01-28 10:04:23 -0800

    Mild refactoring of FieldConstants & YearFieldConstantable

--- src/main/java/com/aembot/lib/constants/FieldConstants.java
+++ src/main/java/com/aembot/lib/constants/FieldConstants.java
@@ -1,8 +1,5 @@
 package com.aembot.lib.constants;
 
-import com.aembot.lib.constants.fields.YearFieldConstantable;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.util.Units;
 
 public class FieldConstants {
@@ -11,24 +8,4 @@ public class FieldConstants {
 
   /** The width dimension of an april tag in meters. */
   public static final double APRIL_TAG_WIDTH_METERS = Units.inchesToMeters(6.5);
-
-  public static Pose3d getAprilTagPose3d(int id, YearFieldConstantable field) {
-    if (0 > id || id > field.getNumTags()) {
-      throw new IllegalArgumentException(
-          String.format("ID must be between 1 and %d. Was %d", field.getNumTags(), id));
-    }
-
-    return field
-        .getFieldLayout()
-        .getTagPose(id)
-        .orElseThrow(
-            () -> {
-              throw new RuntimeException(
-                  String.format("getTagPose called for unexpected tag %d", id));
-            });
-  }
-
-  public static Pose2d getAprilTagPose2d(int id, YearFieldConstantable field) {
-    return getAprilTagPose3d(id, field).toPose2d();
-  }
 }
--- src/main/java/com/aembot/lib/constants/fields/YearFieldConstantable.java
+++ src/main/java/com/aembot/lib/constants/fields/YearFieldConstantable.java
@@ -1,8 +1,8 @@
 package com.aembot.lib.constants.fields;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
-
-// TODO Determine if this interface is useful
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 
 /** Interface for describing a field. */
 public interface YearFieldConstantable {
@@ -12,5 +12,24 @@ public interface YearFieldConstantable {
   public default int getNumTags() {
     final int numTags = getFieldLayout().getTags().size();
     return numTags;
+  }
+
+  public default Pose3d getAprilTagPose3d(int id) {
+    if (0 > id || id > this.getNumTags()) {
+      throw new IllegalArgumentException(
+          String.format("ID must be between 1 and %d. Was %d", this.getNumTags(), id));
+    }
+
+    return this.getFieldLayout()
+        .getTagPose(id)
+        .orElseThrow(
+            () -> {
+              throw new RuntimeException(
+                  String.format("getTagPose called for unexpected tag %d", id));
+            });
+  }
+
+  public default Pose2d getAprilTagPose2d(int id) {
+    return getAprilTagPose3d(id).toPose2d();
   }
 }
