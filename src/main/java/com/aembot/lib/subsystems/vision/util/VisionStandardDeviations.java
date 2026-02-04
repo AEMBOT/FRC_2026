6879e910db5a1b40d0373a787ec95746a8b9684e
Ace5tar <tace233@gmail.com>
2026-02-04 02:14:56 -0800

    fleshsed out vision standard deviations helper class

---
+++ src/main/java/com/aembot/lib/subsystems/vision/util/VisionStandardDeviations.java
@@ -0,0 +1,123 @@
+package com.aembot.lib.subsystems.vision.util;
+
+/** Helper class to store standard deviation values for robot pose */
+public class VisionStandardDeviations {
+
+  /** x component of the standard deviation in position */
+  public final double xStdDev;
+
+  /** y component of the standard deviation in position */
+  public final double yStdDev;
+
+  /** z component of the standard deviation in position */
+  public final double zStdDev;
+
+  /** roll component of the standard devian in rotation */
+  public final double rollStdDev;
+
+  /** pitch component of the standard devian in rotation */
+  public final double pitchStdDev;
+
+  /** yaw component of the standard devian in rotation */
+  public final double yawStdDev;
+
+  /**
+   * Create a new storage class for standard deviations
+   *
+   * @param xStdDev x component of the standard deviation in position
+   * @param yStdDev y component of the standard deviation in position
+   * @param zStdDev z component of the standard deviation in position
+   * @param rollStdDev roll component of the standard devian in rotation
+   * @param pitchStdDev pitch component of the standard devian in rotation
+   * @param yawStdDev yaw component of the standard devian in rotation
+   */
+  public VisionStandardDeviations(
+      double xStdDev,
+      double yStdDev,
+      double zStdDev,
+      double rollStdDev,
+      double pitchStdDev,
+      double yawStdDev) {
+    this.xStdDev = xStdDev;
+    this.yStdDev = yStdDev;
+    this.zStdDev = zStdDev;
+    this.rollStdDev = rollStdDev;
+    this.pitchStdDev = pitchStdDev;
+    this.yawStdDev = yawStdDev;
+  }
+
+  /**
+   * Create a new standard deviations object from an array of values
+   *
+   * @param stdDevsArray array of standard deviations
+   * @return new VisionStandardDeviations object
+   */
+  public static VisionStandardDeviations fromDoubleArray(double[] stdDevsArray) {
+    return new VisionStandardDeviations(
+        stdDevsArray[0],
+        stdDevsArray[1],
+        stdDevsArray[2],
+        stdDevsArray[3],
+        stdDevsArray[4],
+        stdDevsArray[5]);
+  }
+
+  /**
+   * Create a new VisionStandardDeviations object with arbitrarily high value
+   *
+   * <p>Meant to be used to generate std devs for an estimate that sees no tags
+   *
+   * @return new VisionStandardDeviation object
+   */
+  public static VisionStandardDeviations ifSeesNoTags() {
+    return new VisionStandardDeviations(
+        Double.MAX_VALUE,
+        Double.MAX_VALUE,
+        Double.MAX_VALUE,
+        Double.MAX_VALUE,
+        Double.MAX_VALUE,
+        Double.MAX_VALUE);
+  }
+
+  /**
+   * @return Standard deviations of the x position
+   */
+  public double getXStdDev() {
+    return xStdDev;
+  }
+
+  /**
+   * @return Standard deviations of the y position
+   */
+  public double getYStdDev() {
+    return yStdDev;
+  }
+
+  /**
+   * @return Standard deviations of the y position
+   */
+  public double getZStdDev() {
+    return zStdDev;
+  }
+
+  /**
+   * @return Standard deviations of the roll angle
+   */
+  public double getRollStdDev() {
+    return rollStdDev;
+  }
+
+  /**
+   * @return Standard deviations of the pitch angle
+   */
+  public double getPitchStdDev() {
+    return pitchStdDev;
+  }
+
+  /**
+   * @return Standard deviations of the yaw angle
+   */
+  public double getYawStdDev() {
+    return yawStdDev;
+  }
+}
--- src/main/java/com/aembot/lib/subsystems/vision/util/VisionStandardDevs.java
+++
@@ -1,9 +0,0 @@
-package com.aembot.lib.subsystems.vision.util;
-
-public record VisionStandardDevs(
-    double xStdDev,
-    double yStdDev,
-    double zStdDev,
-    double rollStdDev,
-    double pitchStdDev,
-    double yawStdDev) {}
