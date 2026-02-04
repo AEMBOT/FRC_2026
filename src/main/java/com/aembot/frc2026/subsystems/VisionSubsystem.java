fb3035cdf229fc406dbf7d4b2cc97d38a7f3fa8b
Ace5tar <tace233@gmail.com>
2026-02-04 02:16:56 -0800

    implemented vision subsystem

---
+++ src/main/java/com/aembot/frc2026/subsystems/VisionSubsystem.java
@@ -0,0 +1,68 @@
+package com.aembot.frc2026.subsystems;
+
+import com.aembot.frc2026.state.RobotStateYearly;
+import com.aembot.lib.subsystems.base.AEMSubsystem;
+import com.aembot.lib.subsystems.vision.VisionInputs;
+import com.aembot.lib.subsystems.vision.limelight.LimelightIO;
+import com.aembot.lib.subsystems.vision.util.VisionPoseEstimate;
+import edu.wpi.first.math.Pair;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.littletonrobotics.junction.Logger;
+
+public class VisionSubsystem extends AEMSubsystem {
+
+  private final List<Pair<LimelightIO, VisionInputs>> limelightsWithInputsList = new ArrayList<>();
+  private final List<VisionPoseEstimate> limelightPoseEstimates;
+
+  public VisionSubsystem(LimelightIO... limelights) {
+    super("VisionSubsystem");
+
+    for (LimelightIO limelight : limelights) {
+      limelightsWithInputsList.add(Pair.of(limelight, new VisionInputs()));
+    }
+
+    limelightPoseEstimates = new ArrayList<>(limelightsWithInputsList.size());
+  }
+
+  @Override
+  public void periodic() {
+    limelightPoseEstimates.clear();
+
+    for (Pair<LimelightIO, VisionInputs> limelightWithInput : limelightsWithInputsList) {
+      VisionInputs inputs = limelightWithInput.getSecond();
+
+      if (inputs.hasTag) {
+        VisionPoseEstimate poseEstimate =
+            new VisionPoseEstimate(
+                inputs.estimatedRobotPose,
+                RobotStateYearly.get().getLastUsedMegatagTimestamp(),
+                inputs.numTags,
+                inputs.stdDevs);
+        limelightPoseEstimates.add(poseEstimate);
+      }
+    }
+
+    Optional<VisionPoseEstimate> fusedPose = Optional.empty();
+
+    if (!limelightPoseEstimates.isEmpty()) {
+      fusedPose =
+          VisionPoseEstimate.fused(
+              (timestamp) -> RobotStateYearly.get().getFieldRobotPoseAtTime(timestamp),
+              limelightPoseEstimates);
+    }
+
+    if (fusedPose.isPresent()) {
+      RobotStateYearly.get().addMegatagEstimateMeasurement(fusedPose.get());
+    }
+
+    updateLog();
+  }
+
+  @Override
+  public void updateLog(String standardPrefix, String inputPrefix) {
+    Logger.recordOutput(
+        standardPrefix + "/EstimatedMegatagPose", RobotStateYearly.get().getLatestPoseEstimate());
+  }
+}
