1de6e9692125a2370a6756b48fcdc70e8159641d
Ace5tar <tace233@gmail.com>
2026-02-04 03:43:51 -0800

    update robotruntimeconstants to have robot config

--- src/main/java/com/aembot/frc2026/constants/RobotRuntimeConstants.java
+++ src/main/java/com/aembot/frc2026/constants/RobotRuntimeConstants.java
@@ -1,5 +1,6 @@
 package com.aembot.frc2026.constants;
 
+import com.aembot.frc2026.config.RobotConfiguration;
 import com.aembot.frc2026.config.RobotIDYearly;
 import com.aembot.lib.config.RobotID;
 import com.aembot.lib.constants.RuntimeConstants;
@@ -7,5 +8,6 @@ import com.aembot.lib.constants.RuntimeConstants;
 public class RobotRuntimeConstants extends RuntimeConstants {
   public static final RobotID ROBOT_ID = RobotIDYearly.getIdentification();
 
-  // TODO config
+  public static final RobotConfiguration ROBOT_CONFIG =
+      RobotConfiguration.getRobotConstants((RobotIDYearly) ROBOT_ID);
 }
