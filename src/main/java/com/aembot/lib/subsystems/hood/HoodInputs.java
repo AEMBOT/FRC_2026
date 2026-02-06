f644aa7dc91e2eaacff2839b33caebd0a38603cf
Ace5tar <tace233@gmail.com>
2026-02-05 22:18:32 -0800

    added inputs

--- src/main/java/com/aembot/lib/subsystems/hood/HoodInputs.java
+++ src/main/java/com/aembot/lib/subsystems/hood/HoodInputs.java
@@ -1,5 +1,15 @@
 package com.aembot.lib.subsystems.hood;
 
-public class HoodInputs {
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
 
+public class HoodInputs implements LoggableInputs {
+
+  @Override
+  public void toLog(LogTable table) {
+    table.put("isLogging", true);
+  }
+
+  @Override
+  public void fromLog(LogTable table) {}
 }
