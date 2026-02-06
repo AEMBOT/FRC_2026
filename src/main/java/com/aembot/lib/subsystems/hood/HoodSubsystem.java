4fde0a48a8e4cd7026c5c536cc72fa44aca4230a
Ace5tar <tace233@gmail.com>
2026-02-05 22:23:28 -0800

    hood subsystem

--- src/main/java/com/aembot/lib/subsystems/hood/HoodSubsystem.java
+++ src/main/java/com/aembot/lib/subsystems/hood/HoodSubsystem.java
@@ -1,28 +1,40 @@
 package com.aembot.lib.subsystems.hood;
 
+import com.aembot.lib.config.motors.MotorConfiguration;
 import com.aembot.lib.config.subsystems.hood.TalonFXHoodConfiguration;
-import com.aembot.lib.subsystems.base.AEMSubsystem;
-import com.aembot.lib.subsystems.base.MotorCANCoderSubsystem;
+import com.aembot.lib.core.motors.MotorInputs;
+import com.aembot.lib.core.motors.interfaces.MotorIO;
 import com.aembot.lib.subsystems.base.MotorSubsystem;
-import com.aembot.lib.subsystems.hood.io.TalonFXHoodHardwareIO;
+import com.aembot.lib.subsystems.hood.io.HoodIO;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import org.littletonrobotics.junction.Logger;
 
-public class HoodSubsystem extends MotorSubsystem {
+public class HoodSubsystem
+    extends MotorSubsystem<MotorInputs, MotorIO, MotorConfiguration<TalonFXConfiguration>> {
 
-    public HoodSubsystem(
-        TalonFXHoodConfiguration config,
-        TalonFXHoodHardwareIO io
-    ) {
-        super(config.kName, , io.getMotor(), config.kMotorConfig);
-    }
+  private final HoodInputs inputs;
 
-    @Override
-    public void periodic() {
-        super.periodic();
-    }
+  private final HoodIO io;
 
-    @Override
-    public void updateLog(String standardPrefix, String inputPrefix) {
-        
-    }
-    
+  private final TalonFXHoodConfiguration config;
+
+  public HoodSubsystem(HoodInputs inputs, TalonFXHoodConfiguration config, HoodIO io) {
+    super(config.kName, new MotorInputs(), io.getMotor(), config.kMotorConfig);
+    this.inputs = inputs;
+    this.io = io;
+    this.config = config;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    updateLog();
+    super.periodic();
+  }
+
+  @Override
+  public void updateLog(String standardPrefix, String inputPrefix) {
+    Logger.processInputs(inputPrefix, inputs);
+    super.updateLog(standardPrefix, inputPrefix);
+  }
 }
