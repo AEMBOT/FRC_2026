73e68ca3c5b8f7ab31c01e5751d60e2897e04f62
Ace5tar <tace233@gmail.com>
2026-02-12 14:49:48 -0800

    file missing from last commit

---
+++ src/main/java/com/aembot/lib/subsystems/intake/over_bumper/deploy/io/TalonFXOverBumperIntakeDeployHardwareIO.java
@@ -0,0 +1,35 @@
+package com.aembot.lib.subsystems.intake.over_bumper.deploy.io;
+
+import com.aembot.lib.config.subsystems.intake.over_bumper.deploy.TalonFXOverBumperIntakeDeployConfiguration;
+import com.aembot.lib.core.motors.interfaces.MotorIO;
+import com.aembot.lib.core.motors.io.MotorIOTalonFX;
+import com.aembot.lib.subsystems.intake.over_bumper.deploy.OverBumperIntakeDeployInputs;
+
+public class TalonFXOverBumperIntakeDeployHardwareIO implements OverBumperIntakeDeployIO {
+
+  /** Motor IO to use */
+  private final MotorIOTalonFX motor;
+
+  /** Configuration to use for this io */
+  @SuppressWarnings("unused") // Currently unused but that may change in the future
+  private final TalonFXOverBumperIntakeDeployConfiguration config;
+
+  public TalonFXOverBumperIntakeDeployHardwareIO(
+      TalonFXOverBumperIntakeDeployConfiguration config) {
+    this.motor = new MotorIOTalonFX(config.kMotorConfig);
+    this.config = config;
+  }
+
+  @Override
+  public MotorIO getMotor() {
+    return motor;
+  }
+
+  @Override
+  public void updateInputs(OverBumperIntakeDeployInputs inputs) {
+    updateLog();
+  }
+
+  @Override
+  public void updateLog(String standardPrefix, String inputPrefix) {}
+}
