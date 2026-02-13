c703881cfd210b4e042f3ddf4b350bfaac3d4ee0
Ace5tar <tace233@gmail.com>
2026-02-12 17:43:24 -0800

    added state functionality to intake subsystem

--- src/main/java/com/aembot/lib/subsystems/intake/over_bumper/deploy/OverBumperIntakeDeploySubsystem.java
+++ src/main/java/com/aembot/lib/subsystems/intake/over_bumper/deploy/OverBumperIntakeDeploySubsystem.java
@@ -4,10 +4,12 @@ import com.aembot.lib.config.motors.MotorConfiguration;
 import com.aembot.lib.config.subsystems.intake.over_bumper.deploy.TalonFXOverBumperIntakeDeployConfiguration;
 import com.aembot.lib.core.motors.MotorInputs;
 import com.aembot.lib.core.motors.interfaces.MotorIO;
+import com.aembot.lib.state.subsystems.intake.over_bumper.deploy.OverBumperIntakeDeployState;
 import com.aembot.lib.subsystems.base.MotorSubsystem;
 import com.aembot.lib.subsystems.intake.over_bumper.deploy.io.OverBumperIntakeDeployIO;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import edu.wpi.first.wpilibj2.command.Command;
+import java.util.function.Consumer;
 import org.littletonrobotics.junction.Logger;
 
 /** Extension of the motor subsystem to add over the bumper intake deployment functionality */
@@ -20,6 +22,12 @@ public class OverBumperIntakeDeploySubsystem
   /** Configuration to use for this subsytem */
   private final TalonFXOverBumperIntakeDeployConfiguration config;
 
+  /** State of this subsystem */
+  private final OverBumperIntakeDeployState state;
+
+  /** consumer in order to update values in robot state */
+  private final Consumer<OverBumperIntakeDeployState> stateConsumer;
+
   /**
    * Construct a new over the bumper intake deployment subsystem
    *
@@ -27,14 +35,19 @@ public class OverBumperIntakeDeploySubsystem
    * @param io IO layer to use for this subsystem
    */
   public OverBumperIntakeDeploySubsystem(
-      TalonFXOverBumperIntakeDeployConfiguration config, OverBumperIntakeDeployIO io) {
+      TalonFXOverBumperIntakeDeployConfiguration config,
+      OverBumperIntakeDeployIO io,
+      Consumer<OverBumperIntakeDeployState> stateConsumer) {
     super(config.kName, new MotorInputs(), io.getMotor(), config.kMotorConfig);
     this.io = io;
     this.config = config;
+    this.state = new OverBumperIntakeDeployState();
+    this.stateConsumer = stateConsumer;
   }
 
   /**
-   * @return A command that runs the motor upwards until it reaches a hard stop, then resets encoder position
+   * @return A command that runs the motor upwards until it reaches a hard stop, then resets encoder
+   *     position
    */
   public Command getZeroUpwardCommand() {
     return smartVelocitySetpointCommand(() -> config.kZeroingSpeedDegPerSec)
@@ -45,9 +58,10 @@ public class OverBumperIntakeDeploySubsystem
                   config.kMotorConfig.getUnitsToRotorRotations(config.kUpwardStopAngle));
             });
   }
-  
+
   /**
-   * @return A command that runs the motor downwards until it reaches a hard stop, then resets encoder position
+   * @return A command that runs the motor downwards until it reaches a hard stop, then resets
+   *     encoder position
    */
   public Command getZeroDownwardCommand() {
     return smartVelocitySetpointCommand(() -> -config.kZeroingSpeedDegPerSec)
@@ -59,15 +73,29 @@ public class OverBumperIntakeDeploySubsystem
             });
   }
 
+  private void updateState() {
+
+    state.deployPositionUnits = getCurrentPosition();
+
+    state.isDeployed =
+        state.deployPositionUnits
+            < (config.kMotorConfig.kMaxPositionUnits + config.kMotorConfig.kMinPositionUnits) / 2;
+
+    stateConsumer.accept(state);
+  }
+
   @Override
   public void periodic() {
     super.periodic();
+
+    updateState();
   }
 
   @Override
   public void updateLog(String standardPrefix, String inputPrefix) {
     Logger.processInputs(inputPrefix, inputs);
     io.updateLog(standardPrefix, inputPrefix);
+    state.updateLog(standardPrefix + "/state", inputPrefix);
     super.updateLog(standardPrefix, inputPrefix);
   }
 }
--- src/main/java/com/aembot/lib/subsystems/intake/over_bumper/run/OverBumperIntakeRunSubsystem.java
+++ src/main/java/com/aembot/lib/subsystems/intake/over_bumper/run/OverBumperIntakeRunSubsystem.java
@@ -4,9 +4,11 @@ import com.aembot.lib.config.motors.MotorConfiguration;
 import com.aembot.lib.config.subsystems.intake.over_bumper.run.TalonFXOverBumperIntakeRunConfiguration;
 import com.aembot.lib.core.motors.MotorInputs;
 import com.aembot.lib.core.motors.interfaces.MotorIO;
+import com.aembot.lib.state.subsystems.intake.over_bumper.run.OverBumperIntakeRunState;
 import com.aembot.lib.subsystems.base.MotorSubsystem;
 import com.aembot.lib.subsystems.intake.over_bumper.run.io.OverBumperIntakeRunIO;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import java.util.function.Consumer;
 import org.littletonrobotics.junction.Logger;
 
 /**
@@ -18,9 +20,13 @@ public class OverBumperIntakeRunSubsystem
   /** IO layer to use for this subsystem */
   private final OverBumperIntakeRunIO io;
 
+  private final OverBumperIntakeRunState state;
+
   /** Configuration to use for this subsystem */
   @SuppressWarnings("unused") // Currently unused but that may change in the future
   private final TalonFXOverBumperIntakeRunConfiguration config;
+
+  private final Consumer<OverBumperIntakeRunState> stateConsumer;
 
   /**
    * Construct a new over the bumper intake game piece intaking subsystem
@@ -29,21 +35,37 @@ public class OverBumperIntakeRunSubsystem
    * @param io IO layer to use for this subsystem
    */
   public OverBumperIntakeRunSubsystem(
-      TalonFXOverBumperIntakeRunConfiguration config, OverBumperIntakeRunIO io) {
+      TalonFXOverBumperIntakeRunConfiguration config,
+      OverBumperIntakeRunIO io,
+      Consumer<OverBumperIntakeRunState> stateConsumer) {
     super(config.kName, new MotorInputs(), io.getMotor(), config.kMotorConfig);
     this.io = io;
     this.config = config;
+    this.state = new OverBumperIntakeRunState();
+    this.stateConsumer = stateConsumer;
+  }
+
+  private void updateState() {
+
+    state.angularVelocityUnitsPerMin = getCurrentVelocity();
+
+    state.isActive =
+        state.angularVelocityUnitsPerMin > config.kMotorConfig.getMechanismRotationsToUnits(1);
+
+    stateConsumer.accept(state);
   }
 
   @Override
   public void periodic() {
     super.periodic();
+    updateState();
   }
 
   @Override
   public void updateLog(String standardPrefix, String inputPrefix) {
     Logger.processInputs(inputPrefix, inputs);
     io.updateLog(standardPrefix, inputPrefix);
+    state.updateLog(standardPrefix + "/state", inputPrefix);
     super.updateLog(standardPrefix, inputPrefix);
   }
 }
