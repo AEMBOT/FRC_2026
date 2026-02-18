f45122139d612ba1ea8789311b99ccba23521991
Ace5tar <tace233@gmail.com>
2026-02-17 17:37:00 -0800

    added noise to crtTest unit test

--- src/test/java/CRTTest.java
+++ src/test/java/CRTTest.java
@@ -2,6 +2,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.aembot.frc2026.config.robots.ProductionTurretConfig;
 import com.aembot.frc2026.config.subsystems.TalonFXTurretConfiguration;
+import java.util.Random;
 import org.junit.jupiter.api.Test;
 
 public class CRTTest {
@@ -11,56 +12,20 @@ public class CRTTest {
   void DualEncoderTurretAngleCalculation() {
     // double result = testconfig.solve(0.230769231, 0.176470588);
 
+    Random rand = new Random();
+
     for (double i = 0.; i < 750; i += 0.5) {
 
       double rotorPosition = testconfig.kRealMotorConfig.getUnitsToRotorRotations(i);
 
-      double inputA = (rotorPosition % 17.0) / 17.0;
-      double inputB = (rotorPosition % 13.0) / 13.0;
+      double inputA = ((rotorPosition + rand.nextDouble(-0.01, 0.01)) % 17.0) / 17.0;
+      double inputB = ((rotorPosition + rand.nextDouble(-0.01, 0.01)) % 13.0) / 13.0;
       try {
         System.currentTimeMillis();
-        assertEquals(i, testconfig.getMechanismRotationsFromEncoders(inputA, inputB), 0.01);
+        assertEquals(i, testconfig.getMechanismRotationsFromEncoders(inputA, inputB), 0.25);
       } catch (AssertionError e) {
         System.out.println(e.getMessage());
       }
     }
-    // try {
-    //   assertEquals(3., testconfig.solve(0.230769231, 0.176470588), 0.01); // 3/13, 3/17 so
-    // exactly 3 teeth
-    // } catch (AssertionError e) {
-    //   System.out.println(e.getMessage());
-    // }
-    // try {
-    //   assertEquals(4., testconfig.solve(0.230769231, 0.176470588), 0.01); // purposefully
-    // incorrect expected value
-    // } catch (AssertionError e) {
-    //   System.out.println(e.getMessage());
-    // }
-    // try {
-    //   assertEquals(0, testconfig.solve(0, 0), 0.01); // zerp
-    // } catch (AssertionError e) {
-    //   System.out.println(e.getMessage());
-    // }
-    // try {
-    //   assertEquals(100., testconfig.solve(0.692307692, 0.882352941), 0.01); // big number
-    // } catch (AssertionError e) {
-    //   System.out.println(e.getMessage());
-    // }
-    // try {
-    //   assertEquals(96.23, testconfig.solve(0.402307692, 0.660588235), 0.01); // big number with
-    // decimal
-    // } catch (AssertionError e) {
-    //   System.out.println(e.getMessage());
-    // }
-    // try {
-    //   assertEquals(13., testconfig.solve(0, 0.764705882), 0.01); //
-    // } catch (AssertionError e) {
-    //   System.out.println(e.getMessage());
-    // }
-    // try {
-    //   assertEquals(17., testconfig.solve(0.307692308, 0), 0.01);
-    // } catch (AssertionError e) {
-    //   System.out.println(e.getMessage());
-    // }
   }
 }
