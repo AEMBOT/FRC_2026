6597b5d091462858ecba41b3d233ea8c3d472ea5
FlameFish <mccoa499@gmail.com>
2026-02-18 12:21:29 -0800

    Store status signals for canrange instead of periodically retrieving them

--- src/main/java/com/aembot/lib/core/sensors/timeOfFlight/io/TimeOfFlightIOCANRange.java
+++ src/main/java/com/aembot/lib/core/sensors/timeOfFlight/io/TimeOfFlightIOCANRange.java
@@ -5,7 +5,9 @@ import com.aembot.lib.core.can.CANDeviceID;
 import com.aembot.lib.core.can.interfaces.CANable;
 import com.aembot.lib.core.sensors.timeOfFlight.TimeOfFlightInputs;
 import com.aembot.lib.core.sensors.timeOfFlight.interfaces.TimeOfFlightIO;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.CANrange;
+import edu.wpi.first.units.measure.Distance;
 
 public class TimeOfFlightIOCANRange implements TimeOfFlightIO, CANable {
   protected final TimeOfFlightConfiguration kConfig;
@@ -13,12 +15,18 @@ public class TimeOfFlightIOCANRange implements TimeOfFlightIO, CANable {
   protected final CANrange kCanRange;
   protected final CANDeviceID kId;
 
+  protected final StatusSignal<Distance> kDistanceSignal;
+  protected final StatusSignal<Distance> kStdDevsSignal;
+
   public TimeOfFlightIOCANRange(TimeOfFlightConfiguration config) {
     this.kConfig = config;
     this.kId = config.kCANDeviceID;
     this.kCanRange = new CANrange(kId.getDeviceID(), kId.getBus());
 
     kId.setStatusSignal(kCanRange.getSupplyVoltage());
+
+    kDistanceSignal = kCanRange.getDistance();
+    kStdDevsSignal = kCanRange.getDistanceStdDev();
   }
 
   @Override
@@ -31,8 +39,8 @@ public class TimeOfFlightIOCANRange implements TimeOfFlightIO, CANable {
     // Ignore values when the measurement is zero, as it's invalid. This is mostly to patch an issue
     // in sim.
     if (kCanRange.getDistance().getValueAsDouble() != 0) {
-      inputs.distanceMeters = kCanRange.getDistance().getValueAsDouble();
-      inputs.distanceStdDevMeters = kCanRange.getDistanceStdDev().getValueAsDouble();
+      kDistanceSignal.getValueAsDouble();
+      kStdDevsSignal.getValueAsDouble();
     }
   }
 
