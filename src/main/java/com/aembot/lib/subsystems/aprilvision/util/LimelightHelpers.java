3a7bb880c80e18c56bd18ab95e561f92f4b006c8
FlameFish <mccoa499@gmail.com>
2026-02-24 09:53:08 -0800

    Multithread vision, running most blocking networktables calls in a seperate thread

--- src/main/java/com/aembot/lib/subsystems/aprilvision/util/LimelightHelpers.java
+++ src/main/java/com/aembot/lib/subsystems/aprilvision/util/LimelightHelpers.java
@@ -29,6 +29,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * LimelightHelpers provides static methods and classes for interfacing with Limelight vision
  * cameras in FRC. This library supports all Limelight features including AprilTag tracking, Neural
  * Networks, and standard color/retroreflective tracking.
+ *
+ * @see LimelightExtras
  */
 public class LimelightHelpers {
 
