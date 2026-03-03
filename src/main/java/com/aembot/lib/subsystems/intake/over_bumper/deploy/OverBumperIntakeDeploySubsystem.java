a43172f80aa23dc07e5e468c34eebdcbeb3f74c3
Ace5tar <tace233@gmail.com>
2026-03-03 14:59:29 -0800

    spotless

--- src/main/java/com/aembot/lib/subsystems/intake/over_bumper/deploy/OverBumperIntakeDeploySubsystem.java
+++ src/main/java/com/aembot/lib/subsystems/intake/over_bumper/deploy/OverBumperIntakeDeploySubsystem.java
@@ -12,7 +12,6 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import java.util.function.Consumer;
 import org.littletonrobotics.junction.Logger;
 
