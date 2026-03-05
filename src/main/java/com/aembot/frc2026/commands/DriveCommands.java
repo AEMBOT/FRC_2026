9ce4b7bf325194d5939177e64328f6783a15e787
Belle Biery <157433823+Ace5tar@users.noreply.github.com>
2026-03-04 23:44:30 -0800

    Implement Driver Controller Bindings (#22)
    * added bindings requested by driver
    
    * added tower shot command
    
    * added slow mode
    
    * added indexer reverse functionality
    
    * removed testing println in drive commands
    
    * added intake flick command
    
    * added odometry reset
    
    * added simple kill vision command
    
    * Added auto rotation for going under trench
    
    * added looging vision status
    
    * added heading pid constants to drivetrain config
    
    * replaced race with wait command with timeout
    
    * spotless
    
    * changed name of heading command to be more clear
    
    * changes slow mode speed to slow mode factor
    
    * fixed name of indexer backwards command
    
    * move  indexer command by intake command (same bindings)
    
    * spotless
    
    * switch under trench heading command to Commands.either

--- src/main/java/com/aembot/frc2026/commands/DriveCommands.java
+++ src/main/java/com/aembot/frc2026/commands/DriveCommands.java
@@ -3,16 +3,56 @@ package com.aembot.frc2026.commands;
 import com.aembot.frc2026.constants.RobotRuntimeConstants;
 import com.aembot.lib.subsystems.drive.DriveSubsystem;
 import com.aembot.lib.subsystems.drive.commands.JoystickDriveCommand;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.XboxController;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
 public final class DriveCommands {
   public static JoystickDriveCommand createDriveJoystickCmd(
-      DriveSubsystem subsystem, XboxController driverJoystick) {
+      DriveSubsystem subsystem,
+      XboxController driverJoystick,
+      BooleanSupplier slowModeActiveSupplier) {
     return JoystickDriveCommand.createCommandWithSteer(
         subsystem,
         RobotRuntimeConstants.ROBOT_CONFIG.getDrivetrainConfiguration(),
         driverJoystick::getLeftY,
         driverJoystick::getLeftX,
-        driverJoystick::getRightX);
+        driverJoystick::getRightX,
+        slowModeActiveSupplier);
+  }
+
+  public static JoystickDriveCommand createDriveWithForwardHeadingCommand(
+      DriveSubsystem subsystem,
+      XboxController driverJoystick,
+      BooleanSupplier slowModeActiveSupplier) {
+
+    Supplier<Rotation2d> headingSupplier =
+        () -> RobotRuntimeConstants.isBlueAlliance() ? Rotation2d.kZero : Rotation2d.k180deg;
+
+    return JoystickDriveCommand.createCommandWithHeading(
+        subsystem,
+        RobotRuntimeConstants.ROBOT_CONFIG.getDrivetrainConfiguration(),
+        driverJoystick::getLeftY,
+        driverJoystick::getLeftX,
+        headingSupplier,
+        slowModeActiveSupplier);
+  }
+
+  public static JoystickDriveCommand createDriveWithBackwardHeadingCommand(
+      DriveSubsystem subsystem,
+      XboxController driverJoystick,
+      BooleanSupplier slowModeActiveSupplier) {
+
+    Supplier<Rotation2d> headingSupplier =
+        () -> RobotRuntimeConstants.isBlueAlliance() ? Rotation2d.k180deg : Rotation2d.kZero;
+
+    return JoystickDriveCommand.createCommandWithHeading(
+        subsystem,
+        RobotRuntimeConstants.ROBOT_CONFIG.getDrivetrainConfiguration(),
+        driverJoystick::getLeftY,
+        driverJoystick::getLeftX,
+        headingSupplier,
+        slowModeActiveSupplier);
   }
 }
