a7db458b4b19814804ba22b537dcddef9c521271
goober48 <465897@bsd48.org>
2026-03-14 16:44:18 -0700

    fix unit test

--- src/test/java/CRTTest.java
+++ src/test/java/CRTTest.java
@@ -12,16 +12,21 @@ public class CRTTest {
   void DualEncoderTurretAngleCalculation() {
 
     Random rand = new Random();
+    // unit test params. will test to the maximum amount of teeth rotatable
+    int TestATeeth = 13;
+    int TestBTeeth = 17;
+    int TestMechTeeth = 100;
+    for (double i = 0.; i < TestATeeth * TestBTeeth; i += 0.05) {
 
-    for (double i = 0.; i < 750; i += 0.05) {
-
-      double rotorPosition = testconfig.kRealMotorConfig.getUnitsToRotorRotations(i);
-
-      double inputA = ((rotorPosition + rand.nextDouble(-0.01, 0.01)) % 17.0) / 17.0;
-      double inputB = ((rotorPosition + rand.nextDouble(-0.01, 0.01)) % 13.0) / 13.0;
+      double inputA = ((i + rand.nextDouble(-0.01, 0.01)) % TestATeeth) / TestATeeth;
+      double inputB = ((i + rand.nextDouble(-0.01, 0.01)) % TestBTeeth) / TestBTeeth;
       try {
         System.currentTimeMillis();
-        assertEquals(i, testconfig.getMechanismRotationsFromEncoders(inputA, inputB), 0.25);
+        assertEquals(
+            i / TestMechTeeth,
+            testconfig.getMechanismRotationsFromEncoders(
+                inputA, inputB, TestATeeth, TestBTeeth, TestMechTeeth),
+            0.25);
       } catch (AssertionError e) {
         System.out.println(e.getMessage());
       }
