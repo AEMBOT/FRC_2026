0e2b7e5b28ee207f1064dc04cfad606613e4c19c
Will Richards <jwilt9000@gmail.com>
2026-03-16 18:34:39 -0700

    Unflip back limelight

--- src/main/java/com/aembot/frc2026/config/robots/ProductionCameraConfig.java
+++ src/main/java/com/aembot/frc2026/config/robots/ProductionCameraConfig.java
@@ -130,8 +130,8 @@ public class ProductionCameraConfig {
                       Units.inchesToMeters(0),
                       Units.inchesToMeters(9.18)),
                   new Rotation3d(
-                      Units.degreesToRadians(180),
-                      Units.degreesToRadians(-27),
+                      Units.degreesToRadians(0),
+                      Units.degreesToRadians(27),
                       Units.degreesToRadians(180))))
           .withDisabledThrottleValue(DISABLED_THROTTLE)
           .withEnabledThrottleValue(ENABLED_THROTTLE)
