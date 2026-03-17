8700b6330a49262637af934745e4790501de457f
FlameFish <mccoa499@gmail.com>
2026-03-16 19:35:09 -0700

    spotless

--- src/main/java/com/aembot/lib/subsystems/aprilvision/AprilVisionSubsystem.java
+++ src/main/java/com/aembot/lib/subsystems/aprilvision/AprilVisionSubsystem.java
@@ -404,7 +404,7 @@ public class AprilVisionSubsystem extends AEMSubsystem {
 
     // Rotation penalty: at 1 rad/s (57 deg/s) -> 1.5x, at 2 rad/s -> 2x
     // Penalizes rotation since MegaTag2 is more sensitive to angular velocity errors
-    //double rotationPenalty = 1.0 + (omegaRadPerSec * 0.5);
+    // double rotationPenalty = 1.0 + (omegaRadPerSec * 0.5);
 
     double rotationPenalty = 1.0;
     stdDev *= translationPenalty * rotationPenalty;
