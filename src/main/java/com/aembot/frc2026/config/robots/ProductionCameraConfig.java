29a65d960f432105bceb148bb40e276dcee146ce
FlameFish <mccoa499@gmail.com>
2026-03-16 19:18:47 -0700

    a

--- src/main/java/com/aembot/frc2026/config/robots/ProductionCameraConfig.java
+++ src/main/java/com/aembot/frc2026/config/robots/ProductionCameraConfig.java
@@ -36,7 +36,7 @@ public class ProductionCameraConfig {
 
   /* ---- TURRET CAM ---- */
   public final CameraConfiguration cameraConfigTurret =
-      CameraConfiguration.makeLimelight4Config("turret")
+      CameraConfiguration.makeLimelight4Config("turret_disabled_no_use") // FIXME
           .withMechanismOrigin(
               () -> {
                 Rotation2d turretYaw = RobotStateYearly.get().turretState.turretYaw.get();
