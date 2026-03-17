fe58303aaf808f6bb4a444862e9e5c4a4ddc02a7
FlameFish <mccoa499@gmail.com>
2026-03-16 23:12:54 -0700

    as much as i'm tempted to we're not putting texas toast in the logs (esp. since it's both texas toat & toast texas, not just texas toast!)

--- src/main/java/com/aembot/frc2026/config/robots/ProductionIntakeConfig.java
+++ src/main/java/com/aembot/frc2026/config/robots/ProductionIntakeConfig.java
@@ -48,7 +48,7 @@ public class ProductionIntakeConfig {
 
   public final double ZEROING_VOLTAGE = 4.0;
 
-  public final String SUBSYSTEM_NAME = "TexasToastSubsystem";
+  public final String SUBSYSTEM_NAME = "IntexerSubsystem";
 
   public final double DEPLOY_GEAR_RATIO = 18400.0 / 243.0;
 
