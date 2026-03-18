cd10087defe3076c111cf67df49f781cf7d027e3
Ash <mccoa499@gmail.com>
2026-03-18 13:15:24 -0700

    fix issue w/ motor names in intake wheels being the same

--- src/main/java/com/aembot/frc2026/config/robots/ProductionIntakeConfig.java
+++ src/main/java/com/aembot/frc2026/config/robots/ProductionIntakeConfig.java
@@ -244,10 +244,10 @@ public class ProductionIntakeConfig {
           .withCANDevice(
               new CANDeviceID(
                   RIGHT_WHEEL_CAN_ID,
-                  SUBSYSTEM_NAME + "LeftWheelMotor",
+                  SUBSYSTEM_NAME + "RightWheelMotor",
                   SUBSYSTEM_NAME + "Wheels",
                   CANDeviceID.CANDeviceType.TALON_FX))
-          .withName(SUBSYSTEM_NAME + "LeftWheelMotor")
+          .withName(SUBSYSTEM_NAME + "RightWheelMotor")
           .withUnitToRotorRotationRatio(Units.rotationsToDegrees(1 / RIGHT_WHEEL_GEAR_RATIO))
           .withMomentOfInertia(0.00025);
 
