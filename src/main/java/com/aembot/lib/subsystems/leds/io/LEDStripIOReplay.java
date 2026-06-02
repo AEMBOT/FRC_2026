1b5b2a41b5d29c12b06ae3ccd6d6f62f1db53be7
Ash <mccoa499@gmail.com>
2026-06-02 14:55:27 -0700

    start implementing LEDs

---
+++ src/main/java/com/aembot/lib/subsystems/leds/io/LEDStripIOReplay.java
@@ -0,0 +1,8 @@
+package com.aembot.lib.subsystems.leds.io;
+
+import com.aembot.lib.subsystems.leds.interfaces.LEDStripIO;
+
+public class LEDStripIOReplay implements LEDStripIO {
+    @Override
+    public void sendCode(char code) {}
+}
